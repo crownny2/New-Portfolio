f27ba9ca8f0dd1bffeda987f8f786a32e9e9bb15
kacrown02 <147463472+crownny2@users.noreply.github.com>
2026-08-19 12:24:13 +0800

    add daily fade project and fix gitignore

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\crown james\Downloads\crown\portfolio\app\layout.tsx
+// File: C:\Users\crown james\Downloads\New-Portfolio\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\crown james\Downloads\crown\portfolio\app\page.tsx
+// File: C:\Users\crown james\Downloads\New-Portfolio\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
